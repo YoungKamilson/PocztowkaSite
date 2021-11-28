7890bb375c1033feb9fb3b968be1384b939e667b
Kamil <lukuku001@gmail.com>
2021-11-28 11:48:54 +0100

    Nav_ClickedMessage

--- index.js
+++ index.js
@@ -14,21 +14,21 @@ function NavClicked(clicked)
 
     if (clicked == 1)
     {
-        console.log("1")
+        console.log("Nav1_Clicked")
         nav1.style.background = "white";
         nav2.style.background = "transparent";
         nav3.style.background = "transparent";
     }
     if (clicked == 2)
     {
-        console.log("2")
+        console.log("Nav2_Clicked")
         nav1.style.background = "transparent";
         nav2.style.background = "white";
         nav3.style.background = "transparent";
     }
     if (clicked == 3)
     {
-        console.log("3")
+        console.log("Nav3_Clicked")
         nav1.style.background = "transparent";
         nav2.style.background = "transparent";
         nav3.style.background = "white";
